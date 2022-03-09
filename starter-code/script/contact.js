4bbb57e1910b15c5bf3e6a176e878194a5b75e04
George-Hammond <91903758+George-Hammond@users.noreply.github.com>
2022-03-09 07:28:17 +0000

    Contact page (#2)
    * initial commit to create contactPage branch
    
    * initial HTML editing and Css styling
    
    * Second commit- Desktop view
    
    * css finish ups and responsiveness

---
+++ starter-code/script/contact.js
@@ -0,0 +1,48 @@
+const form = document.getElementById("form");
+const email = document.getElementById("email");
+const email2 = document.getElementById('email2');
+
+
+form.addEventListener('submit', e=> {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControls = element.parentElement;
+    const errorDisplay = inputControls.querySelector('.error')
+
+    errorDisplay.innerText = message;
+    inputControls.classList.add('error');
+    inputControls.classList.remove('success')
+
+}
+
+const setSuccess = element =>{
+    const inputControls = element.parentElement;
+    const errorDisplay = inputControls.querySelector('.error')
+
+    errorDisplay.innerText = '';
+    inputControls.classList.add('success');
+    inputControls.classList.remove('error');
+}
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+};
+const validateInputs = () => {
+    const emailValue = email.value.trim();
+    const email2Value = email2.value.trim();
+    
+
+    if(emailValue === '') {
+        setError(email, "This field can't be empty")
+    } else if (!isValidEmail(email)) {
+        setError(email, 'Provide a valid email address')
+    } else {
+        setSuccess(email)
+    }
+    
+};
